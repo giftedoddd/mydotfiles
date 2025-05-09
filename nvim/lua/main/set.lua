0e883ece2511ba904f5554da06961150b3bd2806
giftedodd <giftedoddd@gmail.com>
2025-05-09 20:04:01 +0000

    Create set.lua

---
+++ nvim/lua/main/set.lua
@@ -0,0 +1,9 @@
+vim.opt.swapfile = false
+vim.opt.compatible = false
+vim.opt.showmatch = true
+vim.o.number = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.smartindent = true
+vim.o.wrap = false
+vim.o.termguicolors = true
