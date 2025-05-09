7a99538abfa7c78c966b2f599403b55642a8c65a
giftedodd <giftedoddd@gmail.com>
2025-05-09 20:06:38 +0000

    Create init.lua

---
+++ nvim/lua/main/lazy/init.lua
@@ -0,0 +1,19 @@
+return {
+	'tombh/novim-mode',
+	{
+	"lewis6991/gitsigns.nvim",
+	config = function()
+		require("gitsigns").setup()
+	end
+	},
+	"ap/vim-css-color",
+	{
+	'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+	}
+}
